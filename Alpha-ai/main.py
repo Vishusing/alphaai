037b609452301bbdf7c69d97814a6e7abc259b2e
Vishusing <vs299901@gmail.com>
2024-05-16 09:25:24 +0530

    .

--- Alpha-ai/main.py
+++ Alpha-ai/main.py
@@ -30,10 +30,10 @@ r = sr.Recognizer()
 
 speak("Hello sir, i am Alpha.")
 speak(wishme())
-speak("today is " + today_date.strftime("%d") + " of " + today_date.strftime("%B") + " And its currently " + (today_date.strftime("%I")) +(today_date.strftime("%M")) + (today_date.strftime("%p")))
+speak("today is " + today_date.strftime("%d") + " of " + today_date.strftime("%B") + " And its currently " 
+      + (today_date.strftime("%I")) +(today_date.strftime("%M")) + (today_date.strftime("%p")))
 speak("What can i do for you???")
 
-# Main loop
 while True:
     with sr.Microphone() as source:
         r.energy_threshold = 10000
@@ -43,63 +43,59 @@ while True:
         text = r.recognize_google(audio)
         print(text)
 
-        # Stop condition
-        if "stop" in text.lower():
-            break
+    if "stop" in text.lower():
+        break
 
-        # Wikipedia search function:
-        elif "information" in text:
-            speak("you need information related to which topic?")
-            with sr.Microphone() as source:
-                r.energy_threshold = 10000
-                r.adjust_for_ambient_noise(source, 1.2)
-                print("listening...")
-                audio = r.listen(source)
-                topic = r.recognize_google(audio)
+    elif "information" in text:
+        speak("you need information related to which topic?")
+        with sr.Microphone() as source:
+            r.energy_threshold = 10000
+            r.adjust_for_ambient_noise(source, 1.2)
+            print("listening...")
+            audio = r.listen(source)
+            topic = r.recognize_google(audio)
 
-                speak(f"Searching {topic} in wikipedia")
-                print(f"Searching {topic} in wikipedia")
+        speak(f"Searching {topic} in wikipedia")
+        print(f"Searching {topic} in wikipedia")
 
-                # Open Wikipedia and handle information retrieval
-                open_wikipedia(f"https://en.wikipedia.org/wiki/{topic}")
+        # Open Wikipedia and handle information retrieval
+        open_wikipedia(f"https://en.wikipedia.org/wiki/{topic}")
         
-        # Youtube play functionality:
-        elif "play" in text or "video" in text:
-            speak("you want me to play which video??")
-            with sr.Microphone() as source:
-                r.energy_threshold = 10000
-                r.adjust_for_ambient_noise(source, 1.2)
-                print("listening...")
-                audio = r.listen(source)
-                video = r.recognize_google(audio)
+    elif "play" in text or "video" in text:
+        speak("you want me to play which video??")
+        with sr.Microphone() as source:
+            r.energy_threshold = 10000
+            r.adjust_for_ambient_noise(source, 1.2)
+            print("listening...")
+            audio = r.listen(source)
+            video = r.recognize_google(audio)
 
-                speak(f"Playing {video} an youtube")
-                print(f"Playing {video} an youtube")
-                
-                open_youtube(f"https://www.youtube.com/results?search_query={video.replace(' ', '+')}")
+        speak(f"Playing {video} an youtube")
+        print(f"Playing {video} an youtube")
 
-        # news Api function
-        elif "news" in text:
-            speak("On which topic you want to hear news??")
-            with sr.Microphone() as source:
-                r.energy_threshold = 10000
-                r.adjust_for_ambient_noise(source, 1.2)
-                print("listening...")
-                audio = r.listen(source)
-                news_topic = r.recognize_google(audio)
-            news(news_topic)
+        open_youtube(f"https://www.youtube.com/results?search_query={video.replace(' ', '+')}")
 
-        elif "facts" in text:
-           x=randfacts.get_fact()
-           print(x)
-           speak("Did you know that, "+x)
+    elif "news" in text:
+        speak("On which topic you want to hear news??")
+        with sr.Microphone() as source:
+            r.energy_threshold = 10000
+            r.adjust_for_ambient_noise(source, 1.2)
+            print("listening...")
+            audio = r.listen(source)
+            news_topic = r.recognize_google(audio)
+        news(news_topic)
 
-        elif "jokes" in text:
-            speak("On which topic you want to hear jokes??")
-            with sr.Microphone() as source:
-                r.energy_threshold = 10000
-                r.adjust_for_ambient_noise(source, 1.2)
-                print("listening...")
-                audio = r.listen(source)
-                joke_topic = r.recognize_google(audio)
-            jokes(joke_topic)
+    elif "facts" in text:
+        x=randfacts.get_fact()
+        print(x)
+        speak("Did you know that, "+x)
+
+    elif "jokes" in text:
+        speak("On which topic you want to hear jokes??")
+        with sr.Microphone() as source:
+            r.energy_threshold = 10000
+            r.adjust_for_ambient_noise(source, 1.2)
+            print("listening...")
+            audio = r.listen(source)
+            joke_topic = r.recognize_google(audio)
+        jokes(joke_topic)
